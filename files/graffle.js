30b27dae7ec8dd92bbf4483a2fdf1889623452f3
Peter Lacey-Bordeaux <placeybordeaux@gmail.com>
2012-11-26 12:56:12 -0500

    fixed usability bugs

--- files/graffle.js
+++ files/graffle.js
@@ -37,6 +37,7 @@ Raphael.fn.connection = function (obj1, obj2, line, weight, bg) {
         y1 = p[res[0]].y,
         x4 = p[res[1]].x,
         y4 = p[res[1]].y;
+    var t = r.text((x1+x4)/2,(y1+y4)/2,weight).attr({"font-weight": "bold","font-size": 17})
     dx = Math.max(Math.abs(x1 - x4) / 2, 10);
     dy = Math.max(Math.abs(y1 - y4) / 2, 10);
     var x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
@@ -44,6 +45,8 @@ Raphael.fn.connection = function (obj1, obj2, line, weight, bg) {
         x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
         y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
     var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+
+
     if (line && line.line) {
         line.bg && line.bg.attr({path: path});
         line.line.attr({path: path});
@@ -51,7 +54,7 @@ Raphael.fn.connection = function (obj1, obj2, line, weight, bg) {
         var color = typeof line == "string" ? line : "#000";
         var return_obj = {
             bg: bg && bg.split && this.path(path).attr({stroke: bg.split("|")[0], fill: "none", "stroke-width": bg.split("|")[1] || 3}),
-            text: r.text((x1+x4)/2,(y1+y4)/2,weight).attr({"font-weight": "bold","font-size": 15}),
+            text: t,
             line: this.path(path).attr({stroke: "#5A6351", "stroke-width": 3, fill: "none"}),
             from: obj1,
             to: obj2,
@@ -90,7 +93,7 @@ var el;
 window.onload = function () {
         r = Raphael("holder", 640, 480);
 
-        var graphs = [
+        graphs = [
         graph.create([[40,40],[100,40],[40,100],[160,100],[160,180],[400,100],[420,420]],
                 [[0,2],[0,1],[1,3],[3,4],[3,5],[2,3],[4,6],[1,5],[5,6]]),
  
@@ -101,11 +104,11 @@ window.onload = function () {
  
             ];
 
-        var weighted_graphs = [
+        weighted_graphs = [
         graph.create([[40,40],[100,40],[40,100],[160,100],[160,180],[400,100],[420,420]],
                 [[0,2],[0,1],[1,3],[3,4],[3,5],[2,3],[4,6],[1,5],[5,6]],
                 [1,2,3,4,5,6,2,3,2,1]),
-        graph.create([[140,20],[120,80],[160,80],[100,120],[130,120],[170,120]],
+        graph.create([[240,20],[200,120],[280,120],[160,220],[240,220],[300,220]],
                 [[0,2],[0,1],[1,3],[1,4],[2,5]],
                 [5,3,3,4,1,2]),
         graph.create([[340,200],[310,280],[305,210],[260,180],[300,340],[400,120],[420,20]],
@@ -148,6 +151,7 @@ var algo = {};
 algo.create = function () {
     var n = g.nodes[Math.floor(Math.random()*g.nodes.length)];
     n.animate({"opacity": 1}, 500);
+    n.show();
     return {visited: [],
             frontier: [n],
             next_frontier: []
@@ -162,10 +166,13 @@ var Prims = function() {
     temp.animate({"fill-opacity": 1},500);
     for(var i=0;i<g.nodes.length;i++){
         g.nodes[i].animate({"opacity": 1},500);
+        g.nodes[i].show();
     }
     for(var i=0;i<g.edges.length;i++){
         g.edges[i].line.animate({"opacity": 0.6},500);
         g.edges[i].text.animate({"opacity": 1},500);
+        g.edges[i].line.show();
+        g.edges[i].text.show();
     }
     a.click = function (e){
         if('p' in e){
@@ -246,9 +253,12 @@ var reveal_from_node = function (n) {
         var ns = g.neighbors(n);
         for (var i=0;i<ns.length;i++){
             ns[i].animate({"opacity": 1}, 500);
+            ns[i].show();
         }
         var es = g.get_edges(n);
         for (var i=0;i<es.length;i++){
+            es[i].line.show();
+            es[i].text.show();
             es[i].line.animate({"opacity": 1}, 500);
             es[i].text.animate({"opacity": 1}, 500);
         }
@@ -256,7 +266,6 @@ var reveal_from_node = function (n) {
  
 //click function
 var click = function () {
-    console.log(this.id);
     algorithm.click(this);
 };
 
@@ -271,8 +280,8 @@ graph.create = function (ns,es,ws) {
         var n = r.circle(ns[i][0],ns[i][1],radius);
         var color = Raphael.getColor(1);
         n.attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, "opacity": 0});
-        //shapes[i].drag(move, dragger, up);
         n.click(click);
+        n.hide();
         nodes.push(n);
     };
     //edge creation
@@ -286,6 +295,8 @@ graph.create = function (ns,es,ws) {
         c.text.click(click);
         c.line.attr({"opacity": 0});
         c.text.attr({"opacity": 0});
+        c.line.hide();
+        c.text.hide();
         edges.push(c);
     };
     return {nodes: nodes,edges: edges, neighbors: graph.neighbors, get_edges: graph.get_edges};
